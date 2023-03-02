a0e7a1e7031e6ddc4266a5c02629102348c72a33
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2023-03-02 21:19:41 +0200

    oops

--- chat_bot.py
+++ chat_bot.py
@@ -13,7 +13,7 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 def setup_logger(name, log_file, level=logging.INFO):
     """To setup as many loggers as you want"""
 
-    handler = logging.FileHandler(log_file))        
+    handler = logging.FileHandler(log_file)   
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
