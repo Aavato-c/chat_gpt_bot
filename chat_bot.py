3741ac220faadfbd8723ec0b26bc9f0d629cb4f0
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2023-03-02 20:51:13 +0200

    Print to debug

--- chat_bot.py
+++ chat_bot.py
@@ -23,15 +23,12 @@ def chatgpt_bot(user_name):
     os.system('cls' if os.name == 'nt' else 'clear')
     input_text = input(f"{user_name}: ")
     messages.append({"role": "user", "content": input_text})
+    
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": "Who won the world series in 2020?"},
-                {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-                {"role": "user", "content": "Where was it played?"}
-            ]
-        )
+        messages=messages)
+    print(response)
+    print(response['choices'][0]['message']['content'])
     return response
 
 
